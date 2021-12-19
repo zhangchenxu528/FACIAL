100d600b5e0b8bec23600d3830d317cd6b16298c
zhangchenxu528 <92286174+zhangchenxu528@users.noreply.github.com>
2021-12-18 21:29:48 -0600

    Add files via upload

---
+++ face_render/fit_headpose.py
@@ -0,0 +1,79 @@
+import os, sys
+from os.path import join, exists, abspath, dirname
+import numpy as np
+from time import time
+import matplotlib.pyplot as plt
+import cv2
+import face3d
+from face3d import mesh
+from face3d.morphable_model.fit import estimate_expression, estimate_shape, fit_points, fit_points_for_show
+from load_data import BFM 
+from scipy.io import loadmat,savemat
+import pandas as pd
+from scipy.signal import savgol_filter
+import argparse
+
+parser = argparse.ArgumentParser(description='fit_headpose_setting')
+parser.add_argument('--video_path', type=str, default='/content/FACIAL/video_preprocess/train1.mp4')
+parser.add_argument('--csv_path', type=str, default='/content/FACIAL/video_preprocess/train1_openface/train1_512_audio.csv')
+parser.add_argument('--deepface_path', type=str, default='/content/FACIAL/video_preprocess/train1_deep3Dface/train1.npz')
+parser.add_argument('--save_path', type=str, default='/content/FACIAL/video_preprocess/train1_posenew.npz')
+opt = parser.parse_args()
+
+
+# --- 1. load model
+facemodel = BFM()
+n_exp_para = facemodel.exBase.shape[1]
+
+kpt_ind = facemodel.keypoints
+triangles = facemodel.tri
+
+
+trainname = 'test3'
+
+video_path = opt.video_path
+csv_path = opt.csv_path
+csvinfo=pd.read_csv(csv_path)
+num_image = len(csvinfo)
+base = int(csvinfo.iloc[0]['frame'])-1
+deepface_path = opt.deepface_path
+save_path = opt.save_path
+
+
+realparams = np.load(open(deepface_path, 'rb'))
+realparams = realparams['face']
+
+idparams = realparams[0,0:80]
+texparams = realparams[0,144:224]
+gammaparams = realparams[0,227:254]
+
+vc =cv2.VideoCapture(video_path)
+rval,frame = vc.read()
+h = len(frame[:,0,0])
+w = len(frame[0,:,0])
+
+headpose = np.zeros((num_image,258),dtype=np.float32)
+base = int(csvinfo.iloc[0]['frame'])-1
+# --- 2. fit head pose for each frame
+for frame_count in range(1,num_image+1):
+    print(frame_count)
+    subcsvinfo = csvinfo[csvinfo['frame']==frame_count+base]
+    x = np.zeros((68,2),dtype=np.float32)
+    for i in range(68):
+        x[i,0] = subcsvinfo.iloc[0][' x_'+str(i)]-w/2
+        x[i,1] = (h-subcsvinfo.iloc[0][' y_'+str(i)])- h/2 -1
+    X_ind = kpt_ind
+
+    fitted_sp, fitted_ep, fitted_s, fitted_R, fitted_t = fit_points(x, X_ind, facemodel, np.expand_dims(idparams,0), n_ep = n_exp_para, max_iter = 10)
+
+    fitted_angles = mesh.transform.matrix2angle(fitted_R)
+    fitted_angles = np.array([fitted_angles])
+
+    chi_prev = np.concatenate((fitted_angles[0,:],[fitted_s],fitted_t,realparams[frame_count-1,80:144]),axis=0)
+    params = np.concatenate((chi_prev,idparams,texparams,gammaparams),axis=0)
+    headpose[frame_count-1,:] = params
+# additional smooth
+headpose1 = np.zeros((num_image,258),dtype=np.float32)
+headpose1 = savgol_filter(headpose, 5, 3, axis=0)
+
+np.savez(save_path, face = headpose1)
---
+++ face_render/handle_netface.py
@@ -0,0 +1,24 @@
+import os, sys
+import numpy as np
+from scipy.io import loadmat,savemat
+import glob
+from scipy.signal import savgol_filter
+import argparse
+
+parser = argparse.ArgumentParser(description='netface_setting')
+parser.add_argument('--param_folder', type=str, default='../audio2face/data/train3.npz')
+
+opt = parser.parse_args()
+
+param_folder = opt.param_folder
+
+mat_path_list = sorted(glob.glob(os.path.join(param_folder, '*.mat')))
+len_mat = len(mat_path_list)
+faceshape = np.zeros((len_mat, 257),float)
+
+for i in range(1,len_mat+1):
+    faceshape[i-1,:] = loadmat(os.path.join(param_folder,str(i)+'.mat'))['coeff'][0,:]
+
+
+frames_out_path = os.path.join(param_folder,'train1.npz')
+np.savez(frames_out_path, face = faceshape)
